d78b19e4d4c59924d780357d50ad063ee8635ab2
sage-etcher <sage.codes@email.com>
2024-10-14 16:27:09 -0400

    bug fixes

--- src/generate-site/settings.c
+++ src/generate-site/settings.c
@@ -6,7 +6,7 @@
 
 int g_set_logging_mode;
 char *g_set_database;
-char *g_set_fmtfile;
+char *g_set_fmt_file;
 char *g_set_secname;
 char *g_set_sqlquery;
 char *g_set_output_file;
@@ -17,8 +17,8 @@ settings_load_defaults (void)
 {
     g_set_logging_mode = DEFAULT_LOGGING_MODE;
     g_set_database     = DEFAULT_DATABASE;
-    g_set_fmtfile      = DEFAULT_FORMAT_FILE;
-    g_set_secname      = DEAFULT_SECTION_NAME;
+    g_set_fmt_file     = DEFAULT_FORMAT_FILE;
+    g_set_secname      = DEFAULT_SECTION_NAME;
     g_set_sqlquery     = DEFAULT_SQLQUERY;
     g_set_output_file  = NULL;
 
